19d2ab3ec1c89f83f9621ea46c7491d7b4a330d7
11xnt <allenterescencobq@gmail.com>
2022-10-09 14:58:52 +0100

    updated team store file

--- src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/team/TeamStore.kt
+++ src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/team/TeamStore.kt
@@ -1,4 +1,8 @@
 package org.allenterescenco.tournamentmanager.console.models.team
 
 interface TeamStore {
+    fun findAll(): List<TeamModel>
+    fun findOne(id: Long): TeamModel?
+    fun create(team: TeamModel)
+    fun update(team: TeamModel)
 }
