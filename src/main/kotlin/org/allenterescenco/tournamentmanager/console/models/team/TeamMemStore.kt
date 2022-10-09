0366b0b15dbd844010b746e991a98aaf8f571e5c
11xnt <allenterescencobq@gmail.com>
2022-10-09 15:02:32 +0100

    updated team mem store file

--- src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/team/TeamMemStore.kt
+++ src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/team/TeamMemStore.kt
@@ -1,2 +1,45 @@
 package org.allenterescenco.tournamentmanager.console.models.team
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+var lastId = 0L
+
+internal fun getId(): Long {
+    return org.allenterescenco.tournamentmanager.console.models.tournament.lastId++
+}
+
+class TeamMemStore : TeamStore {
+
+    val teams = ArrayList<TeamModel>()
+
+    override fun findAll(): List<TeamModel> {
+        return teams
+    }
+
+    override fun findOne(id: Long) : TeamModel? {
+        var foundTeam: TeamModel? = teams.find { p -> p.id == id }
+        return foundTeam
+    }
+
+    override fun create(team: TeamModel) {
+        team.id = org.allenterescenco.tournamentmanager.console.models.team.getId()
+        teams.add(team)
+        logAll()
+    }
+
+    override fun update(team: TeamModel) {
+        var foundTeam = findOne(team.id!!)
+        if (foundTeam != null) {
+            foundTeam.name = team.name
+            foundTeam.wins = team.wins
+            foundTeam.losses = team.losses
+            foundTeam.winPercentage = team.winPercentage
+            foundTeam.players = team.players
+        }
+    }
+
+    internal fun logAll() {
+        teams.forEach { org.allenterescenco.tournamentmanager.console.models.team.logger.info("${it}") }
+    }
+}
