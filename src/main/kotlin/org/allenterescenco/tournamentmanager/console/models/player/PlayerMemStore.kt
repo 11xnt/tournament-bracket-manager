88893f3df2a9134a4afff8cf955e3796f0195c7e
11xnt <allenterescencobq@gmail.com>
2022-10-10 00:17:55 +0100

    updated player store and player mem store

--- src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/player/PlayerMemStore.kt
+++ src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/player/PlayerMemStore.kt
@@ -18,24 +18,22 @@ class PlayerMemStore : PlayerStore {
     }
 
     override fun findOne(id: Long) : PlayerModel? {
-        var foundTeam: PlayerModel? = players.find { p -> p.id == id }
-        return foundTeam
+        var foundPlayer: PlayerModel? = players.find { p -> p.id == id }
+        return foundPlayer
     }
 
-    override fun create(team: PlayerModel) {
-        team.id = getId()
-        players.add(team)
+    override fun create(player: PlayerModel) {
+        player.id = getId()
+        players.add(player)
         logAll()
     }
 
-    override fun update(team: PlayerModel) {
-        var foundTeam = findOne(team.id!!)
-        if (foundTeam != null) {
-            foundTeam.name = team.name
-            foundTeam.wins = team.wins
-            foundTeam.losses = team.losses
-            foundTeam.winPercentage = team.winPercentage
-            foundTeam.players = team.players
+    override fun update(player: PlayerModel) {
+        var foundPlayer = findOne(player.id!!)
+        if (foundPlayer != null) {
+            foundPlayer.fullName = player.fullName
+            foundPlayer.dOB = player.dOB
+            foundPlayer.playsFor = player.playsFor
         }
     }
 
--- src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/player/PlayerStore.kt
+++ src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/player/PlayerStore.kt
@@ -1,4 +1,8 @@
 package org.allenterescenco.tournamentmanager.console.models.player
 
 interface PlayerStore {
+    fun findAll(): List<PlayerModel>
+    fun findOne(id: Long): PlayerModel?
+    fun create(player: PlayerModel)
+    fun update(player: PlayerModel)
 }
