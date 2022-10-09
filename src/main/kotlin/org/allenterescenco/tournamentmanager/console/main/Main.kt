740cd542bc25de136029d7a635b307ff4f63a63a
11xnt <allenterescencobq@gmail.com>
2022-10-09 16:41:47 +0100

    Added Main controller to let user go between using the tournament and team controllers

--- src/main/kotlin/org/allenterescenco/tournamentmanager/console/main/Main.kt
+++ src/main/kotlin/org/allenterescenco/tournamentmanager/console/main/Main.kt
@@ -6,5 +6,40 @@ import org.allenterescenco.tournamentmanager.console.controllers.TeamController
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
-    TeamController().start()
+    start()
+}
+
+fun start() {
+    var input: Int
+
+    do {
+        input = menu()
+        when (input) {
+            1 -> TournamentController().start()
+            2 -> TeamController().start()
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Tournament Manager Console App" }
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input : String?
+
+    println("MAIN MENU")
+    println(" 1. Enter Tournament Menu")
+    println(" 2. Enter Team Menu")
+    println("-1. Exit")
+    println()
+    print("Enter Option : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
 }
