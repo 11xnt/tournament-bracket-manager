5a0842ec3304493faf131fd3c98623d7720c9c0b
11xnt <allenterescencobq@gmail.com>
2022-10-12 20:47:22 +0100

    improved player model

--- src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/player/PlayerModel.kt
+++ src/main/kotlin/org/allenterescenco/tournamentmanager/console/models/player/PlayerModel.kt
@@ -4,5 +4,5 @@ import org.allenterescenco.tournamentmanager.console.models.team.TeamModel
 data class PlayerModel(var id: Long = 0,
                        var fullName: String = "Default Name",
                        var dOB: String = "01/01/99",
-                       var playsFor: TeamModel = TeamModel(0,"Default Team",0,0,0, arrayListOf())
+                       var playsFor: TeamModel = TeamModel(0,"Default Team",0,0,0, arrayListOf<PlayerModel>())
 )
